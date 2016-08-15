256ad68f32f57bdd189679d533a7ab154d9994d2
NNposso <56160112@go.buu.ac.th>
2016-08-15 18:08:58 +0700

    sus6

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post('/webhook/', function (req, res) {
 		}
 		if (event.postback) {
 			let text = JSON.stringify(event.postback)
-			sendTextMessage(sender, text.substring(0, 200))
+			sendTextMessage(sender, text.substring(0, 100))
 			continue
 		}
 	}
