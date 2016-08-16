574d63bcf720e9dc67b8812ae4728a523085ecaf
NNposso <56160112@go.buu.ac.th>
2016-08-17 03:04:59 +0700

    add folderr

--- index.js
+++ index.js
@@ -95,6 +95,9 @@ app.get('/webhook', function(req, res) {
       else if (event.read){
       	receivedMessageRead(event)
       }
+      else if (event.postback){
+      	receivedPostback(event)
+      }
       
     }
     res.sendStatus(200)
@@ -368,22 +371,22 @@ function receivedMessage(event) {
  * https://developers.facebook.com/docs/messenger-platform/webhook-reference/postback-received
  * 
  */
-// function receivedPostback(event) {
-//   var senderID = event.sender.id
-//   var recipientID = event.recipient.id
-//   var timeOfPostback = event.timestamp
+function receivedPostback(event) {
+  var senderID = event.sender.id
+  var recipientID = event.recipient.id
+  var timeOfPostback = event.timestamp
 
-//   // The 'payload' param is a developer-defined field which is set in a postback 
-//   // button for Structured Messages. 
-//   var payload = event.postback.payload
+  // The 'payload' param is a developer-defined field which is set in a postback 
+  // button for Structured Messages. 
+  var payload = event.postback.payload
 
-//   console.log("Received postback for user %d and page %d with payload '%s' " + 
-//     "at %d", senderID, recipientID, payload, timeOfPostback)
+  console.log("Received postback for user %d and page %d with payload '%s' " + 
+    "at %d", senderID, recipientID, payload, timeOfPostback)
 
-//   // When a postback is called, we'll send a message back to the sender to 
-//   // let them know it was successful
-//   sendTextMessage(senderID, "Postback called")
-// }
+  // When a postback is called, we'll send a message back to the sender to 
+  // let them know it was successful
+  sendTextMessage(senderID, "Postback called")
+}
 
 /*
  * Message Read Event
