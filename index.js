45d6e65f8ddf4657ea546fe381565e516bb73940
NNposso <56160112@go.buu.ac.th>
2016-08-17 01:18:52 +0700

    add folderr

--- index.js
+++ index.js
@@ -219,105 +219,105 @@ app.post('/webhook/', function (req, res) {
  * then we'll simply confirm that we've received the attachment.
  * 
  */
-function receivedMessage(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfMessage = event.timestamp;
-  var message = event.message;
+// function receivedMessage(event) {
+//   var senderID = event.sender.id;
+//   var recipientID = event.recipient.id;
+//   var timeOfMessage = event.timestamp;
+//   var message = event.message;
 
-  console.log("Received message for user %d and page %d at %d with message:", 
-    senderID, recipientID, timeOfMessage);
-  console.log(JSON.stringify(message));
+//   console.log("Received message for user %d and page %d at %d with message:", 
+//     senderID, recipientID, timeOfMessage);
+//   console.log(JSON.stringify(message));
 
-  var isEcho = message.is_echo;
-  var messageId = message.mid;
-  var appId = message.app_id;
-  var metadata = message.metadata;
+//   var isEcho = message.is_echo;
+//   var messageId = message.mid;
+//   var appId = message.app_id;
+//   var metadata = message.metadata;
 
-  // You may get a text or attachment but not both
-  var messageText = message.text;
-  var messageAttachments = message.attachments;
-  var quickReply = message.quick_reply;
+//   // You may get a text or attachment but not both
+//   var messageText = message.text;
+//   var messageAttachments = message.attachments;
+//   var quickReply = message.quick_reply;
 
-  if (isEcho) {
-    // Just logging message echoes to console
-    console.log("Received echo for message %s and app %d with metadata %s", 
-      messageId, appId, metadata);
-    return;
-  } else if (quickReply) {
-    var quickReplyPayload = quickReply.payload;
-    console.log("Quick reply for message %s with payload %s",
-      messageId, quickReplyPayload);
+//   if (isEcho) {
+//     // Just logging message echoes to console
+//     console.log("Received echo for message %s and app %d with metadata %s", 
+//       messageId, appId, metadata);
+//     return;
+//   } else if (quickReply) {
+//     var quickReplyPayload = quickReply.payload;
+//     console.log("Quick reply for message %s with payload %s",
+//       messageId, quickReplyPayload);
 
-    sendTextMessage(senderID, "Quick reply tapped");
-    return;
-  }
+//     sendTextMessage(senderID, "Quick reply tapped");
+//     return;
+//   }
 
-  if (messageText) {
+//   if (messageText) {
 
-    // If we receive a text message, check to see if it matches any special
-    // keywords and send back the corresponding example. Otherwise, just echo
-    // the text we received.
-    switch (messageText) {
-      case 'image':
-        sendImageMessage(senderID);
-        break;
+//     // If we receive a text message, check to see if it matches any special
+//     // keywords and send back the corresponding example. Otherwise, just echo
+//     // the text we received.
+//     switch (messageText) {
+//       case 'image':
+//         sendImageMessage(senderID);
+//         break;
 
-      case 'gif':
-        sendGifMessage(senderID);
-        break;
+//       case 'gif':
+//         sendGifMessage(senderID);
+//         break;
 
-      case 'audio':
-        sendAudioMessage(senderID);
-        break;
+//       case 'audio':
+//         sendAudioMessage(senderID);
+//         break;
 
-      case 'video':
-        sendVideoMessage(senderID);
-        break;
+//       case 'video':
+//         sendVideoMessage(senderID);
+//         break;
 
-      case 'file':
-        sendFileMessage(senderID);
-        break;
+//       case 'file':
+//         sendFileMessage(senderID);
+//         break;
 
-      case 'button':
-        sendButtonMessage(senderID);
-        break;
+//       case 'button':
+//         sendButtonMessage(senderID);
+//         break;
 
-      case 'generic':
-        sendGenericMessage(senderID);
-        break;
+//       case 'generic':
+//         sendGenericMessage(senderID);
+//         break;
 
-      case 'receipt':
-        sendReceiptMessage(senderID);
-        break;
+//       case 'receipt':
+//         sendReceiptMessage(senderID);
+//         break;
 
-      case 'quick reply':
-        sendQuickReply(senderID);
-        break;        
+//       case 'quick reply':
+//         sendQuickReply(senderID);
+//         break;        
 
-      case 'read receipt':
-        sendReadReceipt(senderID);
-        break;        
+//       case 'read receipt':
+//         sendReadReceipt(senderID);
+//         break;        
 
-      case 'typing on':
-        sendTypingOn(senderID);
-        break;        
+//       case 'typing on':
+//         sendTypingOn(senderID);
+//         break;        
 
-      case 'typing off':
-        sendTypingOff(senderID);
-        break;        
+//       case 'typing off':
+//         sendTypingOff(senderID);
+//         break;        
 
-      case 'account linking':
-        sendAccountLinking(senderID);
-        break;
+//       case 'account linking':
+//         sendAccountLinking(senderID);
+//         break;
 
-      default:
-        sendTextMessage(senderID, messageText);
-    }
-  } else if (messageAttachments) {
-    sendTextMessage(senderID, "Message with attachment received");
-  }
-}
+//       default:
+//         sendTextMessage(senderID, messageText);
+//     }
+//   } else if (messageAttachments) {
+//     sendTextMessage(senderID, "Message with attachment received");
+//   }
+// }
 
 
 /*
@@ -327,23 +327,23 @@ function receivedMessage(event) {
  * these fields at https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-delivered
  *
  */
-function receivedDeliveryConfirmation(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var delivery = event.delivery;
-  var messageIDs = delivery.mids;
-  var watermark = delivery.watermark;
-  var sequenceNumber = delivery.seq;
+// function receivedDeliveryConfirmation(event) {
+//   var senderID = event.sender.id;
+//   var recipientID = event.recipient.id;
+//   var delivery = event.delivery;
+//   var messageIDs = delivery.mids;
+//   var watermark = delivery.watermark;
+//   var sequenceNumber = delivery.seq;
 
-  if (messageIDs) {
-    messageIDs.forEach(function(messageID) {
-      console.log("Received delivery confirmation for message ID: %s", 
-        messageID);
-    });
-  }
+//   if (messageIDs) {
+//     messageIDs.forEach(function(messageID) {
+//       console.log("Received delivery confirmation for message ID: %s", 
+//         messageID);
+//     });
+//   }
 
-  console.log("All message before %d were delivered.", watermark);
-}
+//   console.log("All message before %d were delivered.", watermark);
+// }
 
 
 /*
@@ -353,22 +353,22 @@ function receivedDeliveryConfirmation(event) {
  * https://developers.facebook.com/docs/messenger-platform/webhook-reference/postback-received
  * 
  */
-function receivedPostback(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfPostback = event.timestamp;
+// function receivedPostback(event) {
+//   var senderID = event.sender.id;
+//   var recipientID = event.recipient.id;
+//   var timeOfPostback = event.timestamp;
 
-  // The 'payload' param is a developer-defined field which is set in a postback 
-  // button for Structured Messages. 
-  var payload = event.postback.payload;
+//   // The 'payload' param is a developer-defined field which is set in a postback 
+//   // button for Structured Messages. 
+//   var payload = event.postback.payload;
 
-  console.log("Received postback for user %d and page %d with payload '%s' " + 
-    "at %d", senderID, recipientID, payload, timeOfPostback);
+//   console.log("Received postback for user %d and page %d with payload '%s' " + 
+//     "at %d", senderID, recipientID, payload, timeOfPostback);
 
-  // When a postback is called, we'll send a message back to the sender to 
-  // let them know it was successful
-  sendTextMessage(senderID, "Postback called");
-}
+//   // When a postback is called, we'll send a message back to the sender to 
+//   // let them know it was successful
+//   sendTextMessage(senderID, "Postback called");
+// }
 
 /*
  * Message Read Event
@@ -377,17 +377,17 @@ function receivedPostback(event) {
  * https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-read
  * 
  */
-function receivedMessageRead(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
+// function receivedMessageRead(event) {
+//   var senderID = event.sender.id;
+//   var recipientID = event.recipient.id;
 
-  // All messages before watermark (a timestamp) or sequence have been seen.
-  var watermark = event.read.watermark;
-  var sequenceNumber = event.read.seq;
+//   // All messages before watermark (a timestamp) or sequence have been seen.
+//   var watermark = event.read.watermark;
+//   var sequenceNumber = event.read.seq;
 
-  console.log("Received message read event for watermark %d and sequence " +
-    "number %d", watermark, sequenceNumber);
-}
+//   console.log("Received message read event for watermark %d and sequence " +
+//     "number %d", watermark, sequenceNumber);
+// }
 
 /*
  * Account Link Event
@@ -532,66 +532,44 @@ function sendTextMessage(recipientId, messageText) {
     }
   };
 
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: token },
-    method: 'POST',
-    json: messageData
-
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-
-      if (messageId) {
-        console.log("Successfully sent message with id %s to recipient %s", 
-          messageId, recipientId);
-      } else {
-      console.log("Successfully called Send API for recipient %s", 
-        recipientId);
-      }
-    } else {
-      console.error(response.error);
-    }
-  });  
+  callSendAPI(messageData);
 }
-
 
 /*
  * Send a button message using the Send API.
  *
  */
-function sendButtonMessage(recipientId) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "button",
-          text: "This is test text",
-          buttons:[{
-            type: "web_url",
-            url: "https://www.oculus.com/en-us/rift/",
-            title: "Open Web URL"
-          }, {
-            type: "postback",
-            title: "Trigger Postback",
-            payload: "DEVELOPED_DEFINED_PAYLOAD"
-          }, {
-            type: "phone_number",
-            title: "Call Phone Number",
-            payload: "+16505551234"
-          }]
-        }
-      }
-    }
-  };  
+// function sendButtonMessage(recipientId) {
+//   var messageData = {
+//     recipient: {
+//       id: recipientId
+//     },
+//     message: {
+//       attachment: {
+//         type: "template",
+//         payload: {
+//           template_type: "button",
+//           text: "This is test text",
+//           buttons:[{
+//             type: "web_url",
+//             url: "https://www.oculus.com/en-us/rift/",
+//             title: "Open Web URL"
+//           }, {
+//             type: "postback",
+//             title: "Trigger Postback",
+//             payload: "DEVELOPED_DEFINED_PAYLOAD"
+//           }, {
+//             type: "phone_number",
+//             title: "Call Phone Number",
+//             payload: "+16505551234"
+//           }]
+//         }
+//       }
+//     }
+//   };  
 
-  callSendAPI(messageData);
-}
+//   callSendAPI(messageData);
+// }
 
 /*
  * Send a Structured Message (Generic Message type) using the Send API.
@@ -714,87 +692,87 @@ function sendButtonMessage(recipientId) {
  * Send a message with Quick Reply buttons.
  *
  */
-function sendQuickReply(recipientId) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      text: "What's your favorite movie genre?",
-      metadata: "DEVELOPER_DEFINED_METADATA",
-      quick_replies: [
-        {
-          "content_type":"text",
-          "title":"Action",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_ACTION"
-        },
-        {
-          "content_type":"text",
-          "title":"Comedy",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY"
-        },
-        {
-          "content_type":"text",
-          "title":"Drama",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_DRAMA"
-        }
-      ]
-    }
-  };
+// function sendQuickReply(recipientId) {
+//   var messageData = {
+//     recipient: {
+//       id: recipientId
+//     },
+//     message: {
+//       text: "What's your favorite movie genre?",
+//       metadata: "DEVELOPER_DEFINED_METADATA",
+//       quick_replies: [
+//         {
+//           "content_type":"text",
+//           "title":"Action",
+//           "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_ACTION"
+//         },
+//         {
+//           "content_type":"text",
+//           "title":"Comedy",
+//           "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY"
+//         },
+//         {
+//           "content_type":"text",
+//           "title":"Drama",
+//           "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_DRAMA"
+//         }
+//       ]
+//     }
+//   };
 
-  callSendAPI(messageData);
-}
+//   callSendAPI(messageData);
+// }
 
 /*
  * Send a read receipt to indicate the message has been read
  *
  */
-function sendReadReceipt(recipientId) {
-  console.log("Sending a read receipt to mark message as seen");
+// function sendReadReceipt(recipientId) {
+//   console.log("Sending a read receipt to mark message as seen");
 
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    sender_action: "mark_seen"
-  };
+//   var messageData = {
+//     recipient: {
+//       id: recipientId
+//     },
+//     sender_action: "mark_seen"
+//   };
 
-  callSendAPI(messageData);
-}
+//   callSendAPI(messageData);
+// }
 
 /*
  * Turn typing indicator on
  *
  */
-function sendTypingOn(recipientId) {
-  console.log("Turning typing indicator on");
+// function sendTypingOn(recipientId) {
+//   console.log("Turning typing indicator on");
 
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    sender_action: "typing_on"
-  };
+//   var messageData = {
+//     recipient: {
+//       id: recipientId
+//     },
+//     sender_action: "typing_on"
+//   };
 
-  callSendAPI(messageData);
-}
+//   callSendAPI(messageData);
+// }
 
 /*
  * Turn typing indicator off
  *
  */
-function sendTypingOff(recipientId) {
-  console.log("Turning typing indicator off");
+// function sendTypingOff(recipientId) {
+//   console.log("Turning typing indicator off");
 
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    sender_action: "typing_off"
-  };
+//   var messageData = {
+//     recipient: {
+//       id: recipientId
+//     },
+//     sender_action: "typing_off"
+//   };
 
-  callSendAPI(messageData);
-}
+//   callSendAPI(messageData);
+// }
 
 /*
  * Send a message with the account linking call-to-action
@@ -828,30 +806,30 @@ function sendTypingOff(recipientId) {
  * get the message id in a response 
  *
  */
-// function callSendAPI(messageData) {
-//   request({
-//     uri: 'https://graph.facebook.com/v2.6/me/messages',
-//     qs: { access_token: token },
-//     method: 'POST',
-//     json: messageData
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: token },
+    method: 'POST',
+    json: messageData
 
-//   }, function (error, response, body) {
-//     if (!error && response.statusCode == 200) {
-//       var recipientId = body.recipient_id;
-//       var messageId = body.message_id;
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
 
-//       if (messageId) {
-//         console.log("Successfully sent message with id %s to recipient %s", 
-//           messageId, recipientId);
-//       } else {
-//       console.log("Successfully called Send API for recipient %s", 
-//         recipientId);
-//       }
-//     } else {
-//       console.error(response.error);
-//     }
-//   });  
-// }
+      if (messageId) {
+        console.log("Successfully sent message with id %s to recipient %s", 
+          messageId, recipientId);
+      } else {
+      console.log("Successfully called Send API for recipient %s", 
+        recipientId);
+      }
+    } else {
+      console.error(response.error);
+    }
+  });  
+}
 
 // Start server
 // Webhooks must be available via SSL with a certificate signed by a valid 
