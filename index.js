fce0d5632db6153dbc8f32e575aaeb835e61f46c
NNposso <56160112@go.buu.ac.th>
2016-08-17 01:52:59 +0700

    add folderr

--- index.js
+++ index.js
@@ -84,58 +84,58 @@ app.get('/webhook', function(req, res) {
 //         }
 //     }
 //     res.sendStatus(200)
-// })
- app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
-    for (let i = 0; i < messaging_events.length; i++) {
-      let event = req.body.entry[0].messaging[i]
-      let sender = event.sender.id
-      if (event.message && event.message.text) {
-        let text = event.message.text
-        sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-      }
+// // })
+//  app.post('/webhook/', function (req, res) {
+//     let messaging_events = req.body.entry[0].messaging
+//     for (let i = 0; i < messaging_events.length; i++) {
+//       let event = req.body.entry[0].messaging[i]
+//       let sender = event.sender.id
+//       if (event.message && event.message.text) {
+//         let text = event.message.text
+//         sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+//       }
       
-    }
-    res.sendStatus(200)
-  })
-// app.post('/webhook', function (req, res) {
-//   var data = req.body
-
-//   // Make sure this is a page subscription
-//   if (data.object == 'page') {
-//     // Iterate over each entry
-//     // There may be multiple if batched
-//     data.entry.forEach(function(pageEntry) {
-//       var pageID = pageEntry.id
-//       var timeOfEvent = pageEntry.time
-
-//       // Iterate over each messaging event
-//       pageEntry.messaging.forEach(function(messagingEvent) {
-//         if (messagingEvent.optin) {
-//           receivedAuthentication(messagingEvent)
-//         } else if (messagingEvent.message) {
-//           receivedMessage(messagingEvent)
-//         } else if (messagingEvent.delivery) {
-//           receivedDeliveryConfirmation(messagingEvent)
-//         } else if (messagingEvent.postback) {
-//           receivedPostback(messagingEvent)
-//         } else if (messagingEvent.read) {
-//           receivedMessageRead(messagingEvent)
-//         } else if (messagingEvent.account_linking) {
-//           receivedAccountLink(messagingEvent)
-//         } else {
-//           console.log("Webhook received unknown messagingEvent: ", messagingEvent)
-//         }
-//       })
-//     })
-
-//     // Assume all went well.
-//     //
-//     // You must send back a 200, within 20 seconds, to let us know you've 
-//     // successfully received the callback. Otherwise, the request will time out.
+//     }
 //     res.sendStatus(200)
-//   }
-// })
+//   })
+app.post('/webhook', function (req, res) {
+  var data = req.body
+
+  // Make sure this is a page subscription
+  if (data.object == 'page') {
+    // Iterate over each entry
+    // There may be multiple if batched
+    data.entry.forEach(function(pageEntry) {
+      var pageID = pageEntry.id
+      var timeOfEvent = pageEntry.time
+
+      // Iterate over each messaging event
+      pageEntry.messaging.forEach(function(messagingEvent) {
+        if (messagingEvent.optin) {
+          receivedAuthentication(messagingEvent)
+        } else if (messagingEvent.message) {
+          sendTextMessage(messagingEvent)
+        } else if (messagingEvent.delivery) {
+          receivedDeliveryConfirmation(messagingEvent)
+        } else if (messagingEvent.postback) {
+          receivedPostback(messagingEvent)
+        } else if (messagingEvent.read) {
+          receivedMessageRead(messagingEvent)
+        } else if (messagingEvent.account_linking) {
+          receivedAccountLink(messagingEvent)
+        } else {
+          console.log("Webhook received unknown messagingEvent: ", messagingEvent)
+        }
+      })
+    })
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know you've 
+    // successfully received the callback. Otherwise, the request will time out.
+    res.sendStatus(200)
+  }
+})
 
 /*
  * This path is used for account linking. The account linking call-to-action
