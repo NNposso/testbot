78ac388c86c2fe2385e02fc9732053b2e2cc50eb
NNposso <56160112@go.buu.ac.th>
2016-08-17 00:42:01 +0700

    try

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
   config.get('validationToken');
 
 // Generate a page access token for your page from the App Dashboard
-const token = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
-  (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
-  config.get('pageAccessToken');
+const token = process.env.<MESSENGER_PAGE_ACCESS_TOKEN;
+  // (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
+  // config.get('pageAccessToken');
 
 // URL where the app is running (include protocol). Used to point to scripts and 
 // assets located at this address. 
