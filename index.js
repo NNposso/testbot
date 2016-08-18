0b5ab381f2ab7e78b8827e0dcd415a8534928880
NNposso <56160112@go.buu.ac.th>
2016-08-18 15:52:53 +0700

    Modify

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ app.set('port', (process.env.PORT || 5000))
 // app.set('view engine', 'ejs')
 // app.use(bodyParser.json({ verify: verifyRequestSignature }))
 // app.use(express.static('public'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.set('view engine', 'ejs')
+app.use(bodyParser.json({ verify: verifyRequestSignature }))
+app.use(express.static('public'))
 app.get('/', function (req, res) {
     res.send('Hello world, I am a chat bot')
 })
@@ -642,153 +643,153 @@ function sendGenericMessage(recipientId) {
  * Send a receipt message using the Send API.
  *
  */
-// function sendReceiptMessage(recipientId) {
-//   // Generate a random receipt ID as the API requires a unique ID
-//   var receiptId = "order" + Math.floor(Math.random()*1000)
+function sendReceiptMessage(recipientId) {
+  // Generate a random receipt ID as the API requires a unique ID
+  var receiptId = "order" + Math.floor(Math.random()*1000)
 
-//   var messageData = {
-//     recipient: {
-//       id: recipientId
-//     },
-//     message:{
-//       attachment: {
-//         type: "template",
-//         payload: {
-//           template_type: "receipt",
-//           recipient_name: "Peter Chang",
-//           order_number: receiptId,
-//           currency: "USD",
-//           payment_method: "Visa 1234",        
-//           timestamp: "1428444852", 
-//           elements: [{
-//             title: "Oculus Rift",
-//             subtitle: "Includes: headset, sensor, remote",
-//             quantity: 1,
-//             price: 599.00,
-//             currency: "USD",
-//             image_url: SERVER_URL + "/assets/riftsq.png"
-//           }, {
-//             title: "Samsung Gear VR",
-//             subtitle: "Frost White",
-//             quantity: 1,
-//             price: 99.99,
-//             currency: "USD",
-//             image_url: SERVER_URL + "/assets/gearvrsq.png"
-//           }],
-//           address: {
-//             street_1: "1 Hacker Way",
-//             street_2: "",
-//             city: "Menlo Park",
-//             postal_code: "94025",
-//             state: "CA",
-//             country: "US"
-//           },
-//           summary: {
-//             subtotal: 698.99,
-//             shipping_cost: 20.00,
-//             total_tax: 57.67,
-//             total_cost: 626.66
-//           },
-//           adjustments: [{
-//             name: "New Customer Discount",
-//             amount: -50
-//           }, {
-//             name: "$100 Off Coupon",
-//             amount: -100
-//           }]
-//         }
-//       }
-//     }
-//   }
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message:{
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "receipt",
+          recipient_name: "Peter Chang",
+          order_number: receiptId,
+          currency: "USD",
+          payment_method: "Visa 1234",        
+          timestamp: "1428444852", 
+          elements: [{
+            title: "Oculus Rift",
+            subtitle: "Includes: headset, sensor, remote",
+            quantity: 1,
+            price: 599.00,
+            currency: "USD",
+            image_url: SERVER_URL + "/assets/riftsq.png?raw=true"
+          }, {
+            title: "Samsung Gear VR",
+            subtitle: "Frost White",
+            quantity: 1,
+            price: 99.99,
+            currency: "USD",
+            image_url: SERVER_URL + "/assets/gearvrsq.png?raw=true"
+          }],
+          address: {
+            street_1: "1 Hacker Way",
+            street_2: "",
+            city: "Menlo Park",
+            postal_code: "94025",
+            state: "CA",
+            country: "US"
+          },
+          summary: {
+            subtotal: 698.99,
+            shipping_cost: 20.00,
+            total_tax: 57.67,
+            total_cost: 626.66
+          },
+          adjustments: [{
+            name: "New Customer Discount",
+            amount: -50
+          }, {
+            name: "$100 Off Coupon",
+            amount: -100
+          }]
+        }
+      }
+    }
+  }
 
-//   callSendAPI(messageData)
-// }
+  callSendAPI(messageData)
+}
 
 /*
  * Send a message with Quick Reply buttons.
  *
  */
-// function sendQuickReply(recipientId) {
-//   var messageData = {
-//     recipient: {
-//       id: recipientId
-//     },
-//     message: {
-//       text: "What's your favorite movie genre?",
-//       metadata: "DEVELOPER_DEFINED_METADATA",
-//       quick_replies: [
-//         {
-//           "content_type":"text",
-//           "title":"Action",
-//           "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_ACTION"
-//         },
-//         {
-//           "content_type":"text",
-//           "title":"Comedy",
-//           "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY"
-//         },
-//         {
-//           "content_type":"text",
-//           "title":"Drama",
-//           "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_DRAMA"
-//         }
-//       ]
-//     }
-//   }
+function sendQuickReply(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: "What's your favorite movie genre?",
+      metadata: "DEVELOPER_DEFINED_METADATA",
+      quick_replies: [
+        {
+          "content_type":"text",
+          "title":"Action",
+          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_ACTION"
+        },
+        {
+          "content_type":"text",
+          "title":"Comedy",
+          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY"
+        },
+        {
+          "content_type":"text",
+          "title":"Drama",
+          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_DRAMA"
+        }
+      ]
+    }
+  }
 
-//   callSendAPI(messageData)
-// }
+  callSendAPI(messageData)
+}
 
 /*
  * Send a read receipt to indicate the message has been read
  *
  */
-// function sendReadReceipt(recipientId) {
-//   console.log("Sending a read receipt to mark message as seen")
+function sendReadReceipt(recipientId) {
+  console.log("Sending a read receipt to mark message as seen")
 
-//   var messageData = {
-//     recipient: {
-//       id: recipientId
-//     },
-//     sender_action: "mark_seen"
-//   }
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    sender_action: "mark_seen"
+  }
 
-//   callSendAPI(messageData)
-// }
+  callSendAPI(messageData)
+}
 
 /*
  * Turn typing indicator on
  *
  */
-// function sendTypingOn(recipientId) {
-//   console.log("Turning typing indicator on")
+function sendTypingOn(recipientId) {
+  console.log("Turning typing indicator on")
 
-//   var messageData = {
-//     recipient: {
-//       id: recipientId
-//     },
-//     sender_action: "typing_on"
-//   }
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    sender_action: "typing_on"
+  }
 
-//   callSendAPI(messageData)
-// }
+  callSendAPI(messageData)
+}
 
 /*
  * Turn typing indicator off
  *
  */
-// function sendTypingOff(recipientId) {
-//   console.log("Turning typing indicator off")
+function sendTypingOff(recipientId) {
+  console.log("Turning typing indicator off")
 
-//   var messageData = {
-//     recipient: {
-//       id: recipientId
-//     },
-//     sender_action: "typing_off"
-//   }
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    sender_action: "typing_off"
+  }
 
-//   callSendAPI(messageData)
-// }
+  callSendAPI(messageData)
+}
 
 /*
  * Send a message with the account linking call-to-action
