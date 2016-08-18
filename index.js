a854c97a873030624f5057a3dc236743b046d89b
NNposso <56160112@go.buu.ac.th>
2016-08-18 14:35:50 +0700

    generic message

--- index.js
+++ index.js
@@ -591,52 +591,52 @@ function sendTextMessage(recipientId, messageText) {
  * Send a Structured Message (Generic Message type) using the Send API.
  *
  */
-// function sendGenericMessage(recipientId) {
-//   var messageData = {
-//     recipient: {
-//       id: recipientId
-//     },
-//     message: {
-//       attachment: {
-//         type: "template",
-//         payload: {
-//           template_type: "generic",
-//           elements: [{
-//             title: "rift",
-//             subtitle: "Next-generation virtual reality",
-//             item_url: "https://www.oculus.com/en-us/rift/",               
-//             image_url: SERVER_URL + "/assets/rift.png",
-//             buttons: [{
-//               type: "web_url",
-//               url: "https://www.oculus.com/en-us/rift/",
-//               title: "Open Web URL"
-//             }, {
-//               type: "postback",
-//               title: "Call Postback",
-//               payload: "Payload for first bubble",
-//             }],
-//           }, {
-//             title: "touch",
-//             subtitle: "Your Hands, Now in VR",
-//             item_url: "https://www.oculus.com/en-us/touch/",               
-//             image_url: SERVER_URL + "/assets/touch.png",
-//             buttons: [{
-//               type: "web_url",
-//               url: "https://www.oculus.com/en-us/touch/",
-//               title: "Open Web URL"
-//             }, {
-//               type: "postback",
-//               title: "Call Postback",
-//               payload: "Payload for second bubble",
-//             }]
-//           }]
-//         }
-//       }
-//     }
-//   }  
+function sendGenericMessage(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [{
+            title: "rift",
+            subtitle: "Next-generation virtual reality",
+            item_url: "https://www.oculus.com/en-us/rift/",               
+            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/rift/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for first bubble",
+            }],
+          }, {
+            title: "touch",
+            subtitle: "Your Hands, Now in VR",
+            item_url: "https://www.oculus.com/en-us/touch/",               
+            image_url: "http://messengerdemo.parseapp.com/img/gearvr.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/touch/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for second bubble",
+            }]
+          }]
+        }
+      }
+    }
+  }  
 
-//   callSendAPI(messageData)
-// }
+  callSendAPI(messageData)
+}
 
 /*
  * Send a receipt message using the Send API.
